0f253de332413866be33306c3f73bcf092b9a97a
Cesare Quaranta <cesare.quaranta@pronetics.it>
2016-12-31 13:58:49 +0100

    Prime funzionalità di login

--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,54 @@ var express = require('express');
 var app = require('../app');
 var bodyParser = require("body-parser");
 var router = express.Router();
-
+var db= app.get('db');
+var users = db.get('users');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  users.count({}, function (error, count) {
+   if (err) {
+        console.log(err);
+        res.send('Errore collection users:'+err);
+      }else{
+      res.send(count+" Users registred");
+      }
+  });
 });
 router.post('/login', function (req, res) {
-  res.send('Loged in???');
+  console.log(req.body);
+  var rUsername = req.body.username;
+  var rPassword = req.body.password;
+  users.find({'username': rUsername}).toArray(function (err, result) {
+      if (err) {
+        console.log(err);
+        res.send('Errore di login:'+err);
+      } else if (result.length) {
+        if(result.length === 1){
+          if(result.password === rPassword){
+            res.send('User:'+rUsername+' Loged in');
+            console.log('Login User:'+rUsername);
+          }else{
+            res.send('Password errata per '+rUsername);
+          }
+        }else{
+          var emsg = 'Error: Duplicate user '+rUsername;
+          console.log(emsg);
+          res.send(emsg);
+        }
+      } else {
+         res.send('No User:'+rUsername+' found');
+      }
+  });
+  
 });
 router.put('/user', function (req, res) {
   console.log(req.body);
 
-  var db= app.get('db');
-  var users = db.get('users');
-
   // Submit to the DB
   users.insert({
       "username" : req.body.username,
-      "password" : req.body.password
+      "password" : req.body.password,
+      "regDate" : new Date()
   }, function (err, doc) {
     console.log(err);
   });
